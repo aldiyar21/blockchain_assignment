af41307415c1653bc6b081d494262282fdcf5ff1
aldiyar21 <93989996+aldiyar21@users.noreply.github.com>
2023-09-21 18:53:03 +0600

    Update merkle_tree.py

--- merkle_tree.py
+++ merkle_tree.py
@@ -1,1 +1,35 @@
+import hashlib
 
+class m_node:         #class for every node in merkle tree
+    def __init__(self, data):
+        self.data = data       # storing the data of transaction
+        self.left = None       #left child
+        self.right = None      # right child
+
+class MerkleTree:   
+    def __init__(self, trans):
+        self.trans = trans       #initializing list of trans-s in tree
+        self.root = self.createTree(trans)   #save the root of the tree
+
+    def createTree(self, trans):   #function to combine nodes and create tree
+        if len(trans) == 0:
+            return None
+        if len(trans) == 1:
+            return m_node(trans[0])
+
+        nodes = [m_node(tx) for tx in trans]  #create node for every transaction
+
+        while len(nodes) > 1:
+            new_level = []
+            for i in range(0, len(nodes), 2):
+                left = nodes[i]
+                right = nodes[i + 1] if i + 1 < len(nodes) else left
+                comb_data = left.data + right.data   #combine data from both child-nodes
+                combined_hash = hashlib.sha256(comb_data.encode()).hexdigest()  #calculate the hash of this data
+                parent_node = m_node(combined_hash)  #create a parent node with combined hash
+                parent_node.left = left       #set it to the left and right childs
+                parent_node.right = right
+                new_level.append(parent_node)
+            nodes = new_level
+
+        return nodes[0] #return the root node of the merkle tree
